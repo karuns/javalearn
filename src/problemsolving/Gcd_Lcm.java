69b87f83a50799ea4470b3a8071bef46a440dc7d
skarun <skarun@192.168.2.101>
2016-10-17 11:38:45 -0700

    adding GCD LCM

---
+++ src/problemsolving/Gcd_Lcm.java
@@ -0,0 +1,25 @@
+package problemsolving;
+
+import org.testng.annotations.Test;
+
+public class Gcd_Lcm {
+	@Test
+	public void gcd() {
+		System.out.println(getGcd(12,8));
+		System.out.println(getGcd(8,12));
+		System.out.println(getLcm(8,12));
+		System.out.println(getLcm(8,12));
+	}
+	
+	private int getLcm(int i,int j) {
+		return (i*j)/getGcd(i, j) ;
+	}
+	private int getGcd(int i, int j) {
+		if(j==0) {
+			return i;
+		}
+		else {
+			return getGcd(j, i %j);
+		}
+	}
+}
